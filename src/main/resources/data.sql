beb9240d4d8452d9e5e6c1706e3b7700d29b3f25
Sachin <singhsach1312@gmail.com>
2020-09-28 14:41:53 +0530

    Spring Boot Data JPA

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values(101,'Sachin','Java');
+insert into alien values(104,'Amar','Android');
+insert into alien values(105,'Snail','Java');
+insert into alien values(106,'Neeraj','Ruby');
+insert into alien values(107,'Karan','PHP');
